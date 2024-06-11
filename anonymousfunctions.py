4c216dbef5f5e5ecd8249a8f38f941753846c02e
Juci <hajgato.judit@gmail.com>
2024-06-11 12:55:07 +0200

    Extend anonymousfunctions.py

--- anonymousfunctions.py
+++ anonymousfunctions.py
@@ -4,6 +4,8 @@
 # map() - map a function to each element
 # reduce() - reduce to a single value
 
+from functools import reduce
+
 # multiply with 2:
 def sq(n):
     return n*2
@@ -34,3 +36,15 @@ print(list(filter(lambda x:x>2, lst_2)))
 
 # do the numbers greater than 2?
 print(list(map(lambda x:x>2, lst_2)))
+
+# multiply each value of the list and print the result - reduce
+def prod_terms(a):
+    mul = a[0]
+    for i in range(1, len(a)):
+        mul *= a[i]
+    return mul
+a = [1,2,3,4]
+print(prod_terms(a))
+
+a = [1,2,3,4]
+print(reduce(lambda x,y:x*y,a))
