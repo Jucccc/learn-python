1cd8ea0cddfea98ad081ae474c773d9bd7574481
Juci <hajgato.judit@gmail.com>
2024-06-13 16:08:17 +0200

    Extend generators.py

--- generators.py
+++ generators.py
@@ -1,5 +1,7 @@
 # Generator generate iterator
 # use yield() function to cretate generator
+# Advantages: save huge heaps of memory at the cost of having
+# extra execution time for generating the next iterable object
 
 def sq_num(my_list):
     result = []
@@ -14,3 +16,27 @@ print(a)
 #print(next(a))
 for x in a:
     print(x, end=' ')
+
+# Generator comprehension
+
+b = [4,3,2,1]
+gen_com = (x*x for x in a)
+for i in gen_com:
+    print(i, end=' ')
+
+# Fibonacci series
+def fib_series(a):
+    x, y = 0, 1
+    while True:
+        z = x + y
+        if z < a:
+            yield z
+            x, y = y, z
+        else:
+            break
+gen_fib = fib_series(10)
+print(gen_fib)
+print(next(gen_fib))
+print(next(gen_fib))
+print(next(gen_fib))
+print(next(gen_fib))
