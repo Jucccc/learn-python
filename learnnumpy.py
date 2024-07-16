d62b08f62d353204ab7c34fb2590cdd8e75105df
Jucccc <hajgato.judit@gmail.com>
2024-07-16 14:29:11 +0200

    Extend learnnumpy.py

--- learnnumpy.py
+++ learnnumpy.py
@@ -2,3 +2,11 @@ import numpy as np
 
 # create array from a python list:
 arr = np.array([1,2,3,4,5])
+
+# using built-in functions:
+zeros_arr = np.zeros((3,4)) # creates a 3*4 array filled with zeros
+ones_arr = np.ones((2,3)) # filled with ones
+random_arr = np.random((2,2)) # random values
+arr1 = np.arange(0,10,2) # creates an array [0,2,4,6,8]
+arr2 = np.linspace(0,1,5) # creates an array [0. , 0.25 , 0.5 , 0.75 , 1.]
+
